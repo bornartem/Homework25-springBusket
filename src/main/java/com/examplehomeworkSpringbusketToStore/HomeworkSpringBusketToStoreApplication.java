a5df1db9ee7b4e0d40fb594043812adc7098a25a
bornartem <born.artem@gmail.com>
2023-11-13 13:11:07 +0300

    1 commit

---
+++ src/main/java/com/examplehomeworkSpringbusketToStore/HomeworkSpringBusketToStoreApplication.java
@@ -0,0 +1,13 @@
+package com.examplehomeworkSpringbusketToStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringBusketToStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpringBusketToStoreApplication.class, args);
+	}
+
+}
