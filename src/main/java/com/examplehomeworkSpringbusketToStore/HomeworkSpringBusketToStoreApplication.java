718434939d1f487ce95a7351143cfd72ca3ff4c3
bornartem <born.artem@gmail.com>
2023-11-15 20:27:23 +0300

    2 commit

--- src/main/java/com/examplehomeworkSpringbusketToStore/HomeworkSpringBusketToStoreApplication.java
+++ src/main/java/com/examplehomeworkSpringbusketToStore/HomeworkSpringBusketToStoreApplication.java
@@ -1,13 +1,15 @@
 package com.examplehomeworkSpringbusketToStore;
 
+import com.examplehomeworkSpringbusketToStore.service.BagServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeworkSpringBusketToStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeworkSpringBusketToStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkSpringBusketToStoreApplication.class, args);
 
+    }
 }
